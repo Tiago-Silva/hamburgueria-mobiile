c13eb10489046bfedc2fbe77eb55edcc2ccf66b6
Tiago <ti.almeida58@yahoo.com>
2023-12-28 09:53:29 -0300

    feat: product screen

--- src/screens/Products/index.tsx
+++ src/screens/Products/index.tsx
@@ -1,24 +1,63 @@
+import { ImageSourcePropType, ListRenderItem, ListRenderItemInfo } from "react-native";
 import { Card } from "../../components/Card";
 import { Category } from "../../components/Category";
 import { PromotionCard } from "../../components/PromotionCard";
-import { Container } from "./styles";
+import { Container, WrapperProductsList } from "./styles";
 
+export interface Product {
+  idproduto: number,
+  nome: string,
+  valor: number,
+  status: boolean,
+  urlImage: ImageSourcePropType,
+}
 
 export const Products = () => {
+
+  const data: Product[] = [
+    {
+      idproduto: 1,
+      nome: 'Miami',
+      valor: 15.00,
+      status: true,
+      urlImage: require('.../../../assets/Frame39.png')
+    },
+    {
+      idproduto: 2,
+      nome: 'Chicago',
+      valor: 17.00,
+      status: true,
+      urlImage: require('.../../../assets/Frame39.png')
+    },
+    {
+      idproduto: 3,
+      nome: 'Flórida',
+      valor: 22.00,
+      status: true,
+      urlImage: require('../../../assets/Frame39.png')
+    }
+  ];
+
+  const renderItem = ({ item }: ListRenderItemInfo<Product>) => {
+    return (
+      <Card 
+        title={item.nome}
+        amount={item.valor}
+        urlImage={item.urlImage}
+      />
+    );
+  };
+
   return (
     <Container>
       <PromotionCard />
       <Category />
-      <Card 
-        title="Miami"
-        amount={15.00}
-        urlImage={require('../../../assets/Frame39.png')}
+      <WrapperProductsList 
+        data={data}
+        keyExtractor={(item: Product) => item.idproduto}
+        renderItem={renderItem}
       />
-      <Card 
-        title="Miami"
-        amount={15.00}
-        urlImage={require('../../../assets/Frame39.png')}
-      />
+      
     </Container>
   );
 }
--- src/screens/Products/styles.ts
+++ src/screens/Products/styles.ts
@@ -1,8 +1,25 @@
 import styled from "styled-components/native";
-
+import { FlatList } from "react-native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { Product } from ".";
 
 export const Container = styled.View`
   display: flex;
   justify-content: center;
   align-items: center;
+  height: ${RFPercentage(90)}px;
+`;
+
+export const WrapperProductsList = styled(
+  FlatList as new () => FlatList<Product>
+  ).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+      paddingBottom: '10px'
+  }
+})`
+  margin-top: ${RFPercentage(2)}px;
+  margin-bottom: ${RFPercentage(3)}px;
+  margin-left: ${RFPercentage(34)}px;
+  width: 100%;
 `;
