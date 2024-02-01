28473f56cc52da3de7f10c9ca29ae02345943532
Tiago <ti.almeida58@yahoo.com>
2024-01-31 21:41:02 -0300

    doc: api url, home and work

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -1,3 +1,5 @@
+const urlCasa = 'http://192.168.1.100:8080';
+const urlTrabalho = 'http://192.168.20.20:8080';
 
 
-export const API_URL = 'http://192.168.20.20:8080';
+export const API_URL = urlCasa;
