c4f57af85e82667a399a3fc77bd991d545e31839
Tiago <ti.almeida58@yahoo.com>
2024-01-31 21:39:03 -0300

    feat: header show user icon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { 
   Container, 
   Icon, 
@@ -10,22 +10,50 @@ import {
   UserName,
   UserWrapper, 
 } from "./styles";
+import { useAuth } from "../../hooks/auth";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { Loading } from '../../components/Loading';
+import { ActivityIndicator } from "react-native";
+import * as SecureStorage from 'expo-secure-store';
+
+const storageKey = process.env.EXPO_PUBLIC_USER_STORAGE_KEY;
+
 
 export const Header = React.memo (() => {
+  const { userGoogle } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const signOut = async () => {
+    setIsLoading(true);
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      // usuário foi deslogado
+    } catch (error) {
+      console.error(error);
+    } finally {
+      await SecureStorage.deleteItemAsync(storageKey + 'userGoogle');
+      await SecureStorage.deleteItemAsync(storageKey + 'token');
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Container>
       <UserWrapper>
         <UserInfo>
           <Photo 
-            source={{ uri: 'https://avatars.githubusercontent.com/u/9256219?v=4' }}
+            source={{ uri: userGoogle.photo }}
           />
         <User>
           <UserGreeting>Olá</UserGreeting>
-          <UserName>Alonsão Novães</UserName>
+          <UserName>{userGoogle.name}</UserName>
         </User>
         </UserInfo>
-        <LogoutButton onPress={() => {}}>
-          <Icon name='power' />
+        <LogoutButton onPress={signOut} disabled={isLoading}>
+          {
+            isLoading ? <ActivityIndicator /> :<Icon name='power' />
+          }
         </LogoutButton>
 
       </UserWrapper>
