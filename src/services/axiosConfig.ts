feacac4a2b83a08855bb883c16905776ace808cd
Tiago <ti.almeida58@yahoo.com>
2024-02-10 20:29:08 -0300

    refactor: moving refresh token handling to axiosConfig

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -4,6 +4,14 @@ import * as SecureStorage from 'expo-secure-store';
 
 const storageKey = process.env.EXPO_PUBLIC_USER_STORAGE_KEY;
 
+// Axios para rotas não autenticadas
+const publicAxiosInstance = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'User-Agent': 'MOBILLE'
+  }
+});
+
 // Axios para rotas autenticadas
 const authenticatedAxiosInstance = axios.create({
   baseURL: API_URL,
@@ -30,10 +38,39 @@ authenticatedAxiosInstance.interceptors.request.use(
   }
 );
 
-// Axios para rotas não autenticadas
-const publicAxiosInstance = axios.create({
-  baseURL: API_URL,
-  // Outras configurações do Axios, se necessário
-});
+authenticatedAxiosInstance.interceptors.response.use(
+  (response) => {
+    // Se a resposta foi bem-sucedida, simplesmente retorne a resposta
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+
+      const refreshTokenStorage = await SecureStorage.getItem(storageKey + 'refreshToken');
+      
+      if (refreshTokenStorage) {
+        const tokenObject = JSON.parse(refreshTokenStorage);
+        try {
+          const response = await publicAxiosInstance.post('/auth/refresh-token', tokenObject);
+          
+          if (response) {
+            await SecureStorage.setItem(storageKey + 'token', JSON.stringify(response.data.token));
+            await SecureStorage.setItem(storageKey + 'refreshToken', JSON.stringify(response.data.refreshToken));
+            
+            originalRequest.headers.Authorization = `Bearer ${response.data.token}`;
+            // Refaz a requisição original
+            return authenticatedAxiosInstance(originalRequest);
+          }
+        } catch (error) {
+          console.error('error no axiosConfig: ', error);          
+        }
+      }
+    }
+    // Se ocorreu um erro, faça algo com o erro
+    return Promise.reject(error);
+  }
+);
+
 
 export { authenticatedAxiosInstance, publicAxiosInstance };
--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -1,6 +1,7 @@
 import * as SecureStorage from 'expo-secure-store';
 import { Base64 } from 'js-base64';
 import { TokenData } from '../interface/tokenData';
+import { publicAxiosInstance } from './axiosConfig';
 
 const storageKey = process.env.EXPO_PUBLIC_USER_STORAGE_KEY
 
@@ -26,6 +27,21 @@ export const tokenService = {
   
       const decoded: TokenData = JSON.parse(Base64.decode(parts[1]));
       return decoded;
+    }
+  },
+
+  handleRefreshToken: async (): Promise<void> => {
+    const refreshTokenStorage = await SecureStorage.getItem(storageKey + 'refreshToken');
+
+    if (refreshTokenStorage) {
+      const tokenObject = JSON.parse(refreshTokenStorage);
+      const response = await publicAxiosInstance.post('/auth/refresh-token', tokenObject);
+      
+      if (response) {
+        // setAuthToken(JSON.stringify(response.data.token));
+        await SecureStorage.setItem(storageKey + 'token', JSON.stringify(response.data.token));
+        await SecureStorage.setItem(storageKey + 'refreshToken', JSON.stringify(response.data.refreshToken));
+      }
     }
   }
 
