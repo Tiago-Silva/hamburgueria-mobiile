74a22ae936d9538845c30fab3e26d707a310bd7e
Tiago <ti.almeida58@gmail.com>
2024-02-28 10:15:32 -0300

    feat: subtract and remove item and clear itemList

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,17 +1,17 @@
 import { Reducer } from "redux";
 import { produce } from 'immer';
-import { AddItemToCartAction, ICartState } from "./types";
+import { ActionTypes, AddItemToCartAction, CartActions, ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
   items: []
 }
 
-export const cart: Reducer<ICartState, AddItemToCartAction> = (state = INITIAL_STATE, actions) => {
+export const cart: Reducer<ICartState, CartActions> = (state = INITIAL_STATE, actions) => {
   return produce(state, draft => {
     switch (actions.type) {
-      case 'ADD_ITEM_TO_CART': {
-
-        const { item } = actions.payload;
+      case ActionTypes.ADD_ITEM_TO_CART: {
+        const actionWithPayload = actions as AddItemToCartAction;
+        const { item } = actionWithPayload.payload;
 
         const itemInCartIndex = draft.items.findIndex(cartItem => 
           cartItem.idproduto === item.idproduto
@@ -19,10 +19,50 @@ export const cart: Reducer<ICartState, AddItemToCartAction> = (state = INITIAL_S
 
         if (itemInCartIndex >= 0) {
           draft.items[itemInCartIndex].quantidade++;
+          draft.items[itemInCartIndex].total = draft.items[itemInCartIndex].quantidade * draft.items[itemInCartIndex].valor;
         } else {
           draft.items.push(item);
         }
 
+        break;
+      }
+      case ActionTypes.REMOVE_ITEM_FROM_CART: {
+        const actionWithPayload = actions as AddItemToCartAction;
+        const { item } = actionWithPayload.payload;
+
+        const itemInCartIndex = draft.items.findIndex(cartItem => 
+          cartItem.idproduto === item.idproduto
+        );
+
+        if (itemInCartIndex >= 0) {
+          draft.items.splice(itemInCartIndex, 1);
+        }
+
+        break;
+      }
+      case ActionTypes.REDUCE_ITEM_FROM_CART: {
+        const actionWithPayload = actions as AddItemToCartAction;
+        const { item } = actionWithPayload.payload;
+
+        const itemInCartIndex = draft.items.findIndex(cartItem => 
+          cartItem.idproduto === item.idproduto
+        );
+
+        if (itemInCartIndex >= 0) {
+          draft.items[itemInCartIndex].quantidade--;
+
+          if (draft.items[itemInCartIndex].quantidade <= 0) {
+            draft.items.splice(itemInCartIndex, 1);
+            return;
+          }
+          
+          draft.items[itemInCartIndex].total = draft.items[itemInCartIndex].quantidade * draft.items[itemInCartIndex].valor;
+        }
+
+        break;
+      }
+      case ActionTypes.CLEAR_CART: {
+        draft.items = [];
         break;
       }
       default:
--- src/store/modules/cart/types.ts
+++ src/store/modules/cart/types.ts
@@ -1,5 +1,12 @@
 import { ItemData } from '../../../interface/ItemData';
 
+export enum ActionTypes {
+  ADD_ITEM_TO_CART = 'ADD_ITEM_TO_CART',
+  REMOVE_ITEM_FROM_CART = 'REMOVE_ITEM_FROM_CART',
+  REDUCE_ITEM_FROM_CART = 'REDUCE_ITEM_FROM_CART',
+  CLEAR_CART = 'CLEAR_CART'
+}
+
 export interface IState {
   cart: ICartState;
 }
@@ -18,3 +25,10 @@ export interface AddItemToCartAction extends Action {
     item: ItemData;
   };
 }
+
+export interface ClearCartAction extends Action {
+  type: string;
+}
+
+export type CartActions = AddItemToCartAction | ClearCartAction;
+
